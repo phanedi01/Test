93a477b628796663c906d70e9b1cf2cdb1128107
Roma <roma222@gmail.com>
2025-10-20 12:55:37 +0300

    Основы Js пары

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+// console.log("фывафыва");
+// //комент
+
+// console.log(121+23);//сложение
+// console.log(121-23);//вычетание
+// console.log(121*23);//умножение
+// console.log(121/23);//деление
+
+// console.log(2**2);//степень
+// console.log(10 % 2)//остаток деления
+// console.log(10 % 3)//остаток деления
+
+// //1,3 -операнды
+// // + -оператор
+// // 1 + 3 -операция
+// console.log(-3)//-унарная операция
+// console.log(1+2)//бинарная операция
+
+// //=================================================
+
+// console.log(2 + 2 * 2)//6
+
+// console.log(1 + 2 *(4-2) % 2 )
+// //            4   2  1   3
+let a = 3 ;
+const b = 4 ;
+
+a = 6
+// переменную лет можно изменить а переменную конст нет
+
+// 7 примитивных
+
+console.log(a)
+
+let num = a ;// number
+let str  = "txt" ;// string
+
+let bool = true; // bollean: true or  false 
+
+let begInt = 3252354923476237846311111111111112313187224n;// big int (2**53-1)
+
+let c =  null ; // null
+
+let d  = undefined;// undefined
+
+let symbol = Symbol();//symbol
+
+//======================
+
+let obj ={
+    name:"petya",
+    age:20,
+    socialMedia: [ "instagram", "telegram"]
+}//obj
+
+// 1 сложный 
+
+//=======================
+// console.log(3 ** 5)
+// console.log(-8 / (-4))
+// console.log(100 % 3)
+// console.log((3 ** 5) + (-8 / -4) + (100 % 3));
