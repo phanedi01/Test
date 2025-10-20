41d82044d9f975abfddd65ec91984240f4d7ae74
Roma <roma222@gmail.com>
2025-10-20 13:12:47 +0300

    Проверка

---
+++ Rx.js
@@ -0,0 +1,17 @@
+//  'fd'
+//  'FDF'
+//  'FDJKSFJ'
+//  'good day'
+// console.log("start \n end");
+// console.log("start \" end")
+
+//  // \ - символ экранирования 
+
+// // - конкатенация - объединение строк через +
+
+// console.log("a"+"b"+"c");
+
+let a = 'lol'
+let b = 'kek'
+let c = 'hz'
+console.log(a +' '+ b +' '+ c)
